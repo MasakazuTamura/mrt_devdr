3d00e3a9e6963123dc1cc1d772ad60f3c150ef4d
MasakazuTamura <s16c1084ng@s.chibakoudai.jp>
2018-12-26 18:24:35 +0900

    Move some files from test

---
+++ enter_pocheee.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set Ceu
+
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd $script_dir
+
+make
+sudo insmod myled.ko
+sudo chmod 666 /dev/myled0
---
+++ shigoto_nin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set Ceu
+
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd $script_dir
+
+sudo rm /dev/myled0
+sudo rmmod myled
+make clean
